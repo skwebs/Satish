22c71519cc7c7dd4a4eaafe0bd86bc331d384459
Satish Kumar Sharma <skwebserv@gmail.com>
2023-08-17 05:24:04 +0530

    Changed logo and improve spalsh screen

--- app/src/main/java/com/anshumemorial/satish/MainActivity.kt
+++ app/src/main/java/com/anshumemorial/satish/MainActivity.kt
@@ -2,13 +2,30 @@ package com.anshumemorial.satish
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewTreeObserver.OnPreDrawListener
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
+
+//    private var dataLoaded by Delegates.notNull<Boolean>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Thread.sleep(3000)
+        Thread.sleep(1000)
         installSplashScreen()
         setContentView(R.layout.activity_main)
+//        dataLoaded = false
+////        added
+//        val container = findViewById<View>(android.R.id.content)
+//        container.viewTreeObserver.addOnPreDrawListener(object :OnPreDrawListener{
+//            override fun onPreDraw(): Boolean {
+//                if (dataLoaded){
+//                    return false
+//                }else{
+//                    return true
+//                }
+//            }
+//        })
     }
 }
